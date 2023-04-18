99fb12c4914c9eaa27ed8e1e5e29b23b21206e38
Jihun Yu <30788586+wlgns12370@users.noreply.github.com>
2023-04-18 22:57:00 +0900

    Create 1697 -  Hide and Seek.py

---
+++ BOJ/1697 -  Hide and Seek.py
@@ -0,0 +1,27 @@
+import sys
+input = sys.stdin.readline
+from collections import deque
+max_len = 100_001
+
+dx = [-1,1]
+def bfs(x):
+  global k
+  mattrix[x] = 1
+  queue = deque([x])
+  while queue:
+    pop_x = queue.popleft()
+    for i in range(3):
+      if i == 2:
+        nx = pop_x * 2
+      else:
+        nx = pop_x + dx[i]
+      if 0 <= nx < max_len and mattrix[nx] == 0:
+        mattrix[nx] = mattrix[pop_x] + 1
+        queue.append(nx)
+        if nx == k:
+          break 
+
+n,k = map(int,input().split())
+mattrix = [0 for _ in range(max_len)]
+bfs(n)
+print(mattrix[k]-1)
