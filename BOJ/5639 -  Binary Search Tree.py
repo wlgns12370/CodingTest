1a465d162b848ed15aea026e29b14fbd90193180
Jihun Yu <30788586+wlgns12370@users.noreply.github.com>
2023-04-27 22:48:40 +0900

    Add 5639 -  Binary Search Tree.py

---
+++ BOJ/5639 -  Binary Search Tree.py
@@ -0,0 +1,27 @@
+import sys
+input = sys.stdin.readline
+sys.setrecursionlimit(10001)
+
+def post_order(start,end):
+    if start > end:
+        return
+    # 모두 루트보다 작을때 처리하기 위해서 end + 1
+    mid = end + 1
+    for i in range(start+1,end+1):
+        if pre_order[start] < pre_order[i]:
+            mid = i
+            break
+
+    post_order(start+1,mid-1)
+    post_order(mid,end)
+    #root
+    print(pre_order[start])
+
+pre_order = []
+while True:
+    try:
+        pre_order.append(int(input()))
+    except:
+        break
+
+post_order(0,len(pre_order)-1)
