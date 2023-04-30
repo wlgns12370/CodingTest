c09fa256fb2a79817b84a112a9d738f1f0b452e1
Jihun Yu <30788586+wlgns12370@users.noreply.github.com>
2023-04-30 17:27:36 +0900

    Upload 1697 -  Hide and Seek.py

--- BOJ/1697 -  Hide and Seek.py
+++ BOJ/1697 -  Hide and Seek.py
@@ -3,25 +3,22 @@ input = sys.stdin.readline
 from collections import deque
 max_len = 100_001
 
-dx = [-1,1]
 def bfs(x):
-  global k
-  matrix[x] = 1
-  queue = deque([x])
-  while queue:
-    pop_x = queue.popleft()
-    for i in range(3):
-      if i == 2:
-        nx = pop_x * 2
-      else:
-        nx = pop_x + dx[i]
-      if 0 <= nx < max_len and matrix[nx] == 0:
-        matrix[nx] = matrix[pop_x] + 1
-        queue.append(nx)
-        if nx == k:
-          break 
+    global k
+    mattrix[x] = 0
+    queue = deque([x])
 
+    while queue:
+        current_pos= queue.popleft()
+        if current_pos == k:
+            return mattrix[k] 
+
+        for new_pos in (current_pos+1,current_pos-1,2*current_pos):
+            if 0 <= new_pos < max_len and mattrix[new_pos] == -1:
+                mattrix[new_pos] = mattrix[pop_x] + 1
+                queue.append(new_pos)
+                
 n,k = map(int,input().split())
-matrix = [0 for _ in range(max_len)]
-bfs(n)
-print(matrix[k]-1)
+mattrix = [-1 for _ in range(max_len)]
+
+print(bfs(n))
