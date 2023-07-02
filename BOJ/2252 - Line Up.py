69bb1e6fcd3ef3558734a67fcf90feafebfc7231
Jihun Yu <30788586+wlgns12370@users.noreply.github.com>
2023-07-02 17:31:28 +0900

    Upload 2252 - Line Up.py

---
+++ BOJ/2252 - Line Up.py
@@ -0,0 +1,31 @@
+import sys
+input = sys.stdin.readline
+from collections import deque
+
+#위상정렬
+def Topological_Sort():
+    queue = deque()
+    for i in range(1,n+1):
+        if indegree[i] == 0:
+            queue.append(i)
+
+    while queue:
+        pop_node = queue.popleft()
+        result.append(pop_node)
+        for node in line[pop_node]:
+            indegree[node] -= 1
+            if indegree[node] == 0:
+                queue.append(node)
+
+result = []
+n,m = map(int,input().split())
+line = [[] for _ in range(n+1)]
+indegree = [0 for _ in range(n+1)]
+for _ in range(m):
+    u,v = map(int,input().split())
+    #단방향 연결
+    line[u].append(v)
+    indegree[v] += 1
+
+Topological_Sort()
+print(*result)
