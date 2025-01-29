a0405e7f88d4f8b8e29fe57b20782fb37a06e20e
wlgns12370 <knu12370@knu.ac.kr>
2025-01-29 11:50:20 +0900

    Upload: K번째수

---
+++ Programmers/Level1/python/K번째수.py
@@ -0,0 +1,7 @@
+def solution(array, commands):
+    answer = []
+    for command in commands:
+        l = array[command[0]-1:command[1]]
+        l.sort()
+        answer.append(l[command[2]-1])
+    return answer
