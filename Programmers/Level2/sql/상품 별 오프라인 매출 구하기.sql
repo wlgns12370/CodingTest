d36bed5332a2eeea88cb868919492c2a02f8c623
wlgns12370 <knu12370@knu.ac.kr>
2025-02-22 13:06:36 +0900

    Upload: 상품 별 오프라인 매출 구하기

---
+++ Programmers/Level2/sql/상품 별 오프라인 매출 구하기.sql
@@ -0,0 +1,4 @@
+select p.PRODUCT_CODE as PRODUCT_CODE, sum(o.SALES_AMOUNT)*p.PRICE as SALES
+from PRODUCT as p join OFFLINE_SALE as o on p.PRODUCT_ID = o.PRODUCT_ID
+group by p.PRODUCT_CODE
+order by SALES desc, PRODUCT_CODE asc;
