d2f2eab6ad3ee669173198befe7bf4d5ff33a33c
Jihun Yu <30788586+wlgns12370@users.noreply.github.com>
2026-04-04 13:14:50 +0900

    [Gold V] Title: 평범한 배낭, Time: 288 ms, Memory: 189920 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
@@ -1,21 +1,21 @@
 import sys
 input = sys.stdin.readline
 
-n,k = map(int,input().split())
-memory = [[0]*(k+1) for _ in range(n+1)]
+n, k = map(int,input().split())
 
-bag = [(0,0)] 
+arr = [[0,0]]
 for _ in range(n):
     w,v = map(int,input().split())
-    bag.append((w,v))
+    arr.append([w,v])
 
-for j in range(1,n+1):
-    w,v = bag[j]
-    for i in range(1,k+1):
-        # weight가 더 큰 경우
-        if w > i:
-            memory[j][i] = memory[j-1][i]
+dp = [[0 for _ in range(k+1)] for _ in range(n+1)]
+
+for i in range(1,n+1):
+    w,v = arr[i]
+    for max_w in range(1,k+1):
+        if max_w < w:
+            dp[i][max_w] = dp[i-1][max_w]
         else:
-            memory[j][i] = max(memory[j-1][i] , memory[j-1][i-w]+v)
-
-print(memory[n][k])
+            dp[i][max_w] = max(dp[i-1][max_w], dp[i-1][max_w-w]+v)
+        
+print(dp[n][k])
