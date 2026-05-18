de9ffccf965757a82022e96a9efd9410d6d60761
Jihun Yu <30788586+wlgns12370@users.noreply.github.com>
2026-05-18 15:57:12 +0900

    [level 4] Title: 징검다리, Time: 101.99 ms, Memory: 12 MB -BaekjoonHub

--- 프로그래머스/4/43236. 징검다리/징검다리.py
+++ 프로그래머스/4/43236. 징검다리/징검다리.py
@@ -3,9 +3,7 @@ def solution(distance, rocks, n):
     rocks.sort()
     rocks = [0] + rocks + [distance]
     l = len(rocks)
-            
-    # 최소 거리가 되기 위해서 제거해야하는 바위의 개수를 확인해 바위를 둘 수 있는 지 판단
-    # 거리의 최솟값이 되기 위해서 제거해야하는 돌의 개수와 n비교
+    
     def isOk(k):
         cnt = 0
         prev = 0
@@ -17,16 +15,12 @@ def solution(distance, rocks, n):
                 prev = rocks[i]    
         return cnt <= n
     
-    # 이분탐색
     lo = 0
     hi = 1000000000
-    # 바위 사이의 거리의 최솟값
     answer = 0
     while lo <= hi:
         mid = (lo+hi) // 2
-        temp = isOk(mid)
-        print(mid, temp)
-        if temp:
+        if isOk(mid):
             answer = mid
             lo = mid+1
         else:
